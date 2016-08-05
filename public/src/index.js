1bb440b5ecdadceb3f7d10b786b516e5debfd391
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2016-08-05 12:17:29 +0100

    introduce coords smoothing introduce better sound production add a simple ui that renders a small circle

--- public/src/index.js
+++ public/src/index.js
@@ -4,6 +4,7 @@ window.onload = function() {
   var video = document.createElement('video');
   var detector;
   var audio = new AudioContext();
+  var smoother = new Smoother([0.9995, 0.9995, 0.9995, 0.9995], [0, 0, 0, 0], 0.1);
 
   window.navigator.webkitGetUserMedia({video: true}, function(stream) {
     try {
@@ -29,12 +30,28 @@ window.onload = function() {
     context.stroke();
   }
 
+  var isDetected = false;
+  var detectedToOff = _.debounce(function () {
+    console.log("detect no fist!");
+    isDetected = false;
+    stopOscillator();
+  }, 500);
+
+  var detectedToOn = function (coord) {
+    console.log("detect fist!");
+    isDetected = true;
+
+    var x = coord[0] + coord[2] / 2;
+    var y = coord[1] + coord[3] / 2;
+    createOscillator({clientX: x, clientY: y})
+  };
+
   function play() {
     window.requestAnimationFrame(play);
     if (video.paused) video.play();
 
     // Draw video overlay:
-    canvas.width = ~~(100 * video.videoWidth / video.videoHeight);
+    canvas.width = Math.floor(100 * video.videoWidth / video.videoHeight);
     canvas.height = 100;
     context.drawImage(video, 0, 0, canvas.clientWidth, canvas.clientHeight);
 
@@ -42,28 +59,32 @@ window.onload = function() {
 
       // Prepare the detector once the video dimensions are known:
       if (!detector) {
-        var width = ~~(140 * video.videoWidth / video.videoHeight);
+        var width = Math.floor(140 * video.videoWidth / video.videoHeight);
         var height = 140;
         detector = new objectdetect.detector(width, height, 1.1, objectdetect.handfist);
       }
 
       // Perform the actual detection:
-      var coords = detector.detect(video, 0.5); //format of coords: [x1, y1, x2, y2] ?
+      var coords = detector.detect(video, 1, 1, false, true); //format of coords: [x1, y1, x2, y2] ?
 
       if (coords[0]) {
+
         var coord = coords[0];
 
         // Rescale coordinates from detector to video coordinate space:
-        coord[0] *= video.videoWidth / detector.canvas.width;
-        coord[1] *= video.videoHeight / detector.canvas.height;
-        coord[2] *= video.videoWidth / detector.canvas.width;
-        coord[3] *= video.videoHeight / detector.canvas.height;
+        coord[0] = coord[0] * video.videoWidth / detector.canvas.width; // x - upper-left corner
+        coord[1] = coord[1] * video.videoHeight / detector.canvas.height; // y - upper-left corner
+        coord[2] = coord[2] * video.videoWidth / detector.canvas.width; // width
+        coord[3] = coord[3] * video.videoHeight / detector.canvas.height; // height
+        smoothedCoords = smoother.smooth(coord)
 
-        highlight(context, coord)
+        detectedToOn(smoothedCoords);
+        detectedToOff();
 
+        highlight(context, smoothedCoords)
         changeFrequency({
-          clientX: coord[0] + coord[2] / 2,
-          clientY: coord[1] + coord[3] / 2
+          clientX: smoothedCoords[0] + smoothedCoords[2] / 2,
+          clientY: smoothedCoords[1] + smoothedCoords[3] / 2
         });
       }
     }
@@ -76,7 +97,6 @@ window.onload = function() {
       oscillator = null;
 
   gainNode.connect(audio.destination);
-  createOscillator({clientX: 70, clientY: 50})
 
 
   function calculateFrequency (mouseXPosition) {
@@ -89,7 +109,6 @@ window.onload = function() {
       if (mouseXPosition > 470) { mouseXPosition = 470; }
 
       var frequency = ((mouseXPosition / 470) * maxFrequency) + minFrequency;
-      console.log('=========', frequency);
       return frequency;
   };
 
@@ -103,19 +122,43 @@ window.onload = function() {
       if (mouseYPosition > 360) { mouseYPosition = 360; }
 
       var gain = 1 - ((mouseYPosition / 360) * maxGain) + minGain;
-      console.log('>>>>>>>>>>', gain);
       return gain;
   };
 
-  function createOscillator (e) {
-    var xPos = e.clientX;
-    var yPos = e.clientY;
+  var drawCircle = function (x, y) {
+    x = x / 640 * document.body.clientWidth;
+    y = y / 480 * document.body.clientHeight;
 
-    oscillator = audio.createOscillator();
-    oscillator.frequency.setTargetAtTime(calculateFrequency(xPos), audio.currentTime, 0.001);
-    gainNode.gain.setTargetAtTime(calculateGain(yPos), audio.currentTime, 0.001);
-    oscillator.connect(gainNode);
-    oscillator.start(audio.currentTime);
+    circle.style.background = '#f2ed63';
+    circle.style.display = 'block';
+    circle.style.left = x + 'px';
+    circle.style.top = y + 'px';
+    circle.style.position = 'absolute';
+    circle.style.width = '20px';
+    circle.style.height = '20px';
+    circle.style.borderRadius = '50%';
+  };
+
+  function createOscillator (e) {
+    if (!oscillator) {
+      var xPos = e.clientX;
+      var yPos = e.clientY;
+
+      drawCircle(xPos, yPos)
+      oscillator = audio.createOscillator();
+      oscillator.frequency.setTargetAtTime(calculateFrequency(xPos), audio.currentTime, 0.001);
+      gainNode.gain.setTargetAtTime(calculateGain(yPos), audio.currentTime, 0.001);
+      oscillator.connect(gainNode);
+      oscillator.start(audio.currentTime);
+    }
+  };
+
+  function stopOscillator () {
+    if (oscillator) {
+        oscillator.stop(audio.currentTime);
+        oscillator.disconnect();
+        oscillator = null;
+    }
   };
 
   function changeFrequency (e) {
@@ -124,5 +167,16 @@ window.onload = function() {
 
     oscillator.frequency.setTargetAtTime(calculateFrequency(xPos), audio.currentTime , 0.001);
     gainNode.gain.setTargetAtTime(calculateGain(yPos), audio.currentTime, 0.001);
+    drawCircle(xPos, yPos);
   };
 };
+
+// debug mode
+var canvas = document.getElementById('canvas');
+if (location.hash === "#debug") {
+  canvas.style.display = "inherit";
+}
+else {
+  canvas.style.display = "none";
+}
+
